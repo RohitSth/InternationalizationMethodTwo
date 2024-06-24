729b6526a6ad8a191eec43811d501c74204c2f1e
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-06-24 18:13:35 +0545

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  i18n: {
+    locales: ["default", "en", "de", "fr"],
+    defaultLocale: "default",
+    localeDetection: false,
+  },
+  trailingSlash: true,
+};
+
 export default nextConfig;
